ef9812afdd9e7f4c314395c1eb113e903c8e6a7e
Taichi Sasaki <tchsskk@gmail.com>
2019-11-26 19:14:00 +0900

    Use strict testdata

--- testdata/src/design/design.go
+++ testdata/src/design/design.go
@@ -1,28 +1,28 @@
 package design
 
 import (
-	. "github.com/goadesign/goa/design"        // want `"github.com/goadesign/goa/design" should be removed`
-	. "github.com/goadesign/goa/design/apidsl" // want `"github.com/goadesign/goa/design/apidsl" should be replaced with "goa.design/goa/v3/dsl"`
+	. "github.com/goadesign/goa/design"        // want `\A"github.com/goadesign/goa/design" should be removed\z`
+	. "github.com/goadesign/goa/design/apidsl" // want `\A"github.com/goadesign/goa/design/apidsl" should be replaced with "goa.design/goa/v3/dsl"\z`
 	"net/http"
 )
 
-var UserMedia = MediaType("application/vnd.user+json", func() { // want `MediaType should be replaced with ResultType`
-	Attribute("id", Integer)                          // want `Integer should be replaced with Int`
-	Attribute("permissions", HashOf(String, Boolean)) // want `HashOf should be replaced with MapOf`
-	Attribute("created_at", DateTime)                 // want `DateTime should be replaced with String`
+var UserMedia = MediaType("application/vnd.user+json", func() { // want `\AMediaType should be replaced with ResultType\z`
+	Attribute("id", Integer)                          // want `\AInteger should be replaced with Int\z`
+	Attribute("permissions", HashOf(String, Boolean)) // want `\AHashOf should be replaced with MapOf\z`
+	Attribute("created_at", DateTime)                 // want `\ADateTime should be replaced with String \+ Format\(FormatDateTime\)\z`
 })
 
-var _ = Resource("user", func() { // want `Resource should be replaced with Service`
-	BasePath("/users")      // want `BasePath should be replaced with Path and move it into HTTP`
-	Action("show", func() { // want `Action should be replaced with Method`
-		Routing(GET("/:user_id"))        // want `Routing should be replaced with HTTP and colons in HTTP routing DSLs should be replaced with curly braces`
-		Response(OK, UserMedia, func() { // want `OK should be replaced with StatusOK and Status should be replaced with Code`
+var _ = Resource("user", func() { // want `\AResource should be replaced with Service\z`
+	BasePath("/users")      // want `\ABasePath should be replaced with Path and move it into HTTP\z`
+	Action("show", func() { // want `\AAction should be replaced with Method\z`
+		Routing(GET("/:user_id"))        // want `\ARouting should be replaced with HTTP and colons in HTTP routing DSLs should be replaced with curly braces\z`
+		Response(OK, UserMedia, func() { // want `\AOK should be replaced with StatusOK and Status should be replaced with Code\z`
 			Status(http.StatusOK)
 		})
-		Metadata("swagger:summary", "Show users") // want `Metadata should be replaced with Meta`
+		Metadata("swagger:summary", "Show users") // want `\AMetadata should be replaced with Meta\z`
 	})
-	Action("list", func() { // want `Action should be replaced with Method`
-		Routing(GET("/"))                     // want `Routing should be replaced with HTTP`
-		Response(OK, CollectionOf(UserMedia)) // want `OK should be replaced with StatusOK`
+	Action("list", func() { // want `\AAction should be replaced with Method\z`
+		Routing(GET("/"))                     // want `\ARouting should be replaced with HTTP\z`
+		Response(OK, CollectionOf(UserMedia)) // want `\AOK should be replaced with StatusOK\z`
 	})
 })
